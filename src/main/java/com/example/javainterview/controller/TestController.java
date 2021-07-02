20291c4ec4fc578a1537456974b1f6fed34ea4d8
Anshu Kumar <anshu.kumar@epay.ingenico.com>
2021-07-02 13:33:21 +0200

    renamed controller

--- src/main/java/com/example/javainterview/controller/TestController.java
+++ src/main/java/com/example/javainterview/controller/TestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class TestController {
 
     @GetMapping("/greeting")
     public String greeting() {
